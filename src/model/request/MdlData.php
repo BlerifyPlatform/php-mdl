462d01eddfa35942f0d60099a14c9d7555c8f40d
Erick Pacheco Pedraza <eum602@users.noreply.github.com>
2025-06-19 19:51:03 -0500

    feat: Add support for nationality and optional properties (#13)
    Signed-off-by: eum602 <eum602@gmail.com>

--- src/model/request/MdlData.php
+++ src/model/request/MdlData.php
@@ -6,75 +6,93 @@ use JsonSerializable;
 
 class MdlData implements JsonSerializable
 {
-    private string $familyName;
-    private string $givenName;
-    private string $birthdate;
-    private string $issueDate;
-    private string $expiryDate;
-    private string $issuingCountry;
+    private ?string $familyName=null;
+    private ?string $givenName=null;
+    private ?string $birthdate=null;
+    private ?string $issueDate=null;
+    private ?string $expiryDate=null;
+    private ?string $issuingCountry=null;
 
-    private string $issuingAuthority;
-    private string $documentNumber;
-    private string $portrait;
+    private ?string $issuingAuthority=null;
+    private ?string $documentNumber=null;
+    private ?string $portrait=null;
 
-    private $drivingPrivileges;
+    private $drivingPrivileges=null;
 
-    private string $unDistinguishingSign;
+    private ?string $unDistinguishingSign=null;
+
+    private ?string $administrativeNumber=null;
+
+    private ?int $sex=null;
+
+    private ?int $height=null;
+    private ?int $weight=null;
+
+    private ?string $eyeColour=null;
+
+    private ?string $birthPlace=null;
+    private ?string $residentAddress=null;
+    private ?string $issuingJurisdiction=null;
+    private ?string $nationality=null;
+    private ?string $residentCity=null;
+    private ?string $residentState=null;
+    private ?string $residentPostalCode=null;
+    private ?string $residentCountry=null;
 
     public static function new(): MdlData
     {
         return new MdlData();
     }
 
-    public function familyName($familyName): MdlData
+    public function familyName(string $familyName): MdlData
     {
         $this->familyName = $familyName;
         return $this;
     }
 
-    public function givenName($givenName): MdlData
+    public function givenName(string $givenName): MdlData
     {
         $this->givenName = $givenName;
         return $this;
     }
 
-    public function birthDate($birthdate): MdlData
+    public function birthDate(string $birthdate): MdlData
     {
         $this->birthdate = $birthdate;
         return $this;
     }
 
-    public function issueDate($issueDate): MdlData
+    public function issueDate(string $issueDate): MdlData
     {
         $this->issueDate = $issueDate;
         return $this;
     }
 
-    public function expiryDate($expiryDate): MdlData
+    public function expiryDate(string $expiryDate): MdlData
     {
         $this->expiryDate = $expiryDate;
         return $this;
     }
 
-    public function issuingCountry($issuingCountry): MdlData
+    public function issuingCountry(string $issuingCountry): MdlData
     {
         $this->issuingCountry = $issuingCountry;
         return $this;
     }
 
-    public function issuingAuthority($authority): MdlData
+    public function issuingAuthority(string $authority): MdlData
     {
         $this->issuingAuthority = $authority;
         return $this;
     }
 
-    public function documentNumber($documentNumber): MdlData
+    public function documentNumber(string $documentNumber): MdlData
     {
         $this->documentNumber = $documentNumber;
         return $this;
     }
 
-    public function portrait($portrait): MdlData
+    public function portrait(string $portrait): MdlData
     {
         $this->portrait = $portrait;
         return $this;
@@ -86,27 +104,165 @@ class MdlData implements JsonSerializable
         return $this;
     }
 
-    public function unDistinguishingSign($unDistinguishingSign): MdlData
+    public function unDistinguishingSign(string $unDistinguishingSign): MdlData
     {
         $this->unDistinguishingSign = $unDistinguishingSign;
         return $this;
     }
 
+    public function administrativeNumber(string $administrativeNumber): MdlData
+    {
+        $this->administrativeNumber = $administrativeNumber;
+        return $this;
+    }
+
+    public function sex(int $sex): MdlData
+    {
+        $this->sex = $sex;
+        return $this;
+    }
+
+    public function height(int $height): MdlData
+    {
+        $this->height = $height;
+        return $this;
+    }
+
+    public function weight(int $weight): MdlData
+    {
+        $this->weight = $weight;
+        return $this;
+    }
+
+    public function eyeColour(string $eyeColour): MdlData
+    {
+        $this->eyeColour = $eyeColour;
+        return $this;
+    }
+
+    public function birthPlace(string $birthPlace): MdlData
+    {
+        $this->birthPlace = $birthPlace;
+        return $this;
+    }
+
+    public function residentAddress(string $residentAddress): MdlData
+    {
+        $this->residentAddress = $residentAddress;
+        return $this;
+    }
+    
+    public function issuingJurisdiction(string $issuingJurisdiction): MdlData
+    {
+        $this->issuingJurisdiction = $issuingJurisdiction;
+        return $this;
+    }
+
+    public function nationality(string $nationality): MdlData
+    {
+        $this->nationality = $nationality;
+        return $this;
+    }
+    public function residentCity(string $residentCity): MdlData
+    {
+        $this->residentCity = $residentCity;
+        return $this;
+    }
+    public function residentState(string $residentState): MdlData
+    {
+        $this->residentState = $residentState;
+        return $this;
+    }
+
+    public function residentPostalCode(string $residentPostalCode): MdlData
+    {
+        $this->residentPostalCode = $residentPostalCode;
+        return $this;
+    }
+    public function residentCountry(string $residentCountry): MdlData
+    {
+        $this->residentCountry = $residentCountry;
+        return $this;
+    }
+
     public function jsonSerialize(): array
     {
-        return [
-            'family_name' => $this->familyName,
-            'given_name' => $this->givenName,
-            'birth_date' => $this->birthdate,
-            'issue_date' => $this->issueDate,
-            'expiry_date' => $this->expiryDate,
-            'issuing_country' => $this->issuingCountry,
-            'issuing_authority' => $this->issuingAuthority,
-            'document_number' => $this->documentNumber,
-            'portrait' => $this->portrait,
-            'driving_privileges' => $this->drivingPrivileges,
-            'un_distinguishing_sign' => $this->unDistinguishingSign
-        ];
+        if ($this->familyName !== null) {
+            $data['family_name'] = $this->familyName;
+        }
+
+        if ($this->givenName !== null) {
+            $data['given_name'] = $this->givenName;
+        }
+        if ($this->birthdate !== null) {
+            $data['birth_date'] = $this->birthdate;
+        }
+        if ($this->issueDate !== null) {
+            $data['issue_date'] = $this->issueDate;
+        }
+        if ($this->expiryDate !== null) {
+            $data['expiry_date'] = $this->expiryDate;
+        }
+        if ($this->issuingCountry !== null) {
+            $data['issuing_country'] = $this->issuingCountry;
+        }
+        if ($this->issuingAuthority !== null) {
+            $data['issuing_authority'] = $this->issuingAuthority;
+        }
+        if ($this->documentNumber !== null) {
+            $data['document_number'] = $this->documentNumber;
+        }
+        if ($this->portrait !== null) {
+            $data['portrait'] = $this->portrait;
+        }
+        if ($this->drivingPrivileges !== null) {
+            $data['driving_privileges'] = $this->drivingPrivileges;
+        }
+        if ($this->unDistinguishingSign !== null) {
+            $data['un_distinguishing_sign'] = $this->unDistinguishingSign;
+        }
+        if ($this->administrativeNumber !== null) {
+            $data['administrative_number'] = $this->administrativeNumber;
+        }
+        if ($this->sex !== null) {
+            $data['sex'] = $this->sex;
+        }
+        if ($this->height !== null) {
+            $data['height'] = $this->height;
+        }
+        if ($this->weight !== null) {
+            $data['weight'] = $this->weight;
+        }
+        if ($this->eyeColour !== null) {
+            $data['eye_colour'] = $this->eyeColour;
+        }
+        if ($this->birthPlace !== null) {
+            $data['birth_place'] = $this->birthPlace;
+        }
+        if ($this->residentAddress !== null) {
+            $data['resident_address'] = $this->residentAddress;
+        }
+        if ($this->issuingJurisdiction !== null) {
+            $data['issuing_jurisdiction'] = $this->issuingJurisdiction;
+        }
+        if ($this->nationality !== null) {
+            $data['nationality'] = $this->nationality;
+        }
+
+        if ($this->residentCity !== null) {
+            $data['resident_city'] = $this->residentCity;
+        }
+        if ($this->residentState !== null) {
+            $data['resident_state'] = $this->residentState;
+        }
+        if ($this->residentPostalCode !== null) {
+            $data['resident_postal_code'] = $this->residentPostalCode;
+        }
+        if ($this->residentCountry !== null) {
+            $data['resident_country'] = $this->residentCountry;
+        }
+
+        return $data;
     }
 
 }
